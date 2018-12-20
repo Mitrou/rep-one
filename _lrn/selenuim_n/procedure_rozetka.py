968ccba577307db0c7f18664ceb4c6f34fa9f52e
Mitrou <mahnosam@gmail.com>
2018-12-20 10:18:18 +0200

    os

---
+++ _lrn/selenuim_n/procedure_rozetka.py
@@ -0,0 +1,24 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from time import sleep
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+
+# configuring profile
+fp = webdriver.FirefoxProfile()
+fp.set_preference('browser.download.folderList', 2)
+fp.set_preference('browser.download.manager.showWhenStarting', False)
+fp.set_preference('browser.download.dir', "C:\\Git\\rep-one\\file_download")
+fp.set_preference('browser.helperApps.neverAsk.saveToDisk', 'application/zip')
+
+
+
+driver = webdriver.Firefox(firefox_profile=fp)
+wait = WebDriverWait(driver, 10)
+driver.get("https://rozetka.com.ua/")
+driver.find_element_by_link_text('увійдіть в особистий кабінет').click()
+# wait.until(EC.visibility_of_element_located((By.ID, 'auth_email')))
+sleep(1)
+driver.find_element_by_id('auth_email').send_keys('mahnosam@gmail.com')
+driver.find_element_by_id('auth_pass').send_keys('Gtkmvtyb1984')
